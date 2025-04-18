63cd279838dabd0f4ca2010a56aa8f073369632e
Edwin <tellez.edwin.a@gmail.com>
2025-04-18 14:12:53 -0500

    dev1 add initial iam resoruces
    dev1 add services account and vars
    
    dev1 add bucket permissions

--- variables.tf
+++ variables.tf
@@ -24,3 +24,58 @@ variable "tags" {
         Owner   = "YourName"
     }
 }
+
+variable "project_id" {
+    description = "The GCP project ID"
+    type        = string
+}
+
+variable "viewer_role" {
+    description = "The role for viewers"
+    type        = string
+}
+
+variable "viewer_member" {
+    description = "The member to assign the viewer role"
+    type        = string
+}
+
+variable "editor_role" {
+    description = "The role for editors"
+    type        = string
+}
+
+variable "editor_members" {
+    description = "The members to assign the editor role"
+    type        = list(string)
+}
+
+variable "owner_role" {
+    description = "The role for owners"
+    type        = string
+}
+
+variable "owner_member" {
+    description = "The member to assign the owner role"
+    type        = string
+}
+
+variable "service_account_id" {
+    description = "The ID of the service account"
+    type        = string
+}
+
+variable "service_account_display_name" {
+    description = "The display name of the service account"
+    type        = string
+}
+
+variable "service_account_description" {
+    description = "The description of the service account"
+    type        = string
+}
+
+variable "storage_admin_role" {
+    description = "The role for storage admin"
+    type        = string
+}
